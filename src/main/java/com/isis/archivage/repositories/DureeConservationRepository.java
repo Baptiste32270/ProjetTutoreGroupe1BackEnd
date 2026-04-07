90b44d34a6f82638761343a51d088d1957338099
Baptiste32270 <baptiste.faugeres@sfr.fr>
2026-04-07 16:43:02 +0200

    crash duree de conservation

--- src/main/java/com/isis/archivage/repositories/DureeConservationRepository.java
+++ src/main/java/com/isis/archivage/repositories/DureeConservationRepository.java
@@ -4,15 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.isis.archivage.entities.DureeConservation;
-import com.isis.archivage.enums.CategorieArchive; // À importer si tu lies la durée à tes catégories
-
-import java.util.Optional;
 import java.util.List;
 
 @Repository
 public interface DureeConservationRepository extends JpaRepository<DureeConservation, Long> {
-
-    Optional<DureeConservation> findByCategorie(CategorieArchive categorie);
 
     List<DureeConservation> findByDureeAnneesGreaterThanEqual(Integer annees);
 }
